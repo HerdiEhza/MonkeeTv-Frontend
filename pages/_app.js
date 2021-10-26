db3f8fbbb5e4a0211ad524cd0952f729096c6105
HerdiEhzaSaputra <herdi.ehza@icloud.com>
2021-10-26 13:54:58 +0700

    fix path env backend

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <link rel="preconnect" href="https://app.snipcart.com" />
         <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <link rel="icon" type="image/ico" href="./public/favicon.ico" />
         <link
           rel="stylesheet"
           href="https://cdn.snipcart.com/themes/v3.0.16/default/snipcart.css"
